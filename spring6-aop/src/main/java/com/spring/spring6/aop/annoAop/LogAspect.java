addaf21ef724c14e34277fced238a4b6d599f2a2
YuYang94724 <the38245691@gmail.com>
2023-05-28 00:58:42 +0800

    feat: 新增LogAspect並使用@Before, @After, @AfterRetuning, @AfterThrowing, @Around的測試

--- spring6-aop/src/main/java/com/spring/spring6/aop/annoAop/LogAspect.java
+++ spring6-aop/src/main/java/com/spring/spring6/aop/annoAop/LogAspect.java
@@ -3,22 +3,23 @@ package com.spring.spring6.aop.annoAop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.List;
 
 //切面類別
 @Component //ioc容器
 @Aspect //切面類
+@Order(0)//優先級別, 數字越小優先級別越高
 public class LogAspect {
 
     //設置切入點和通知類型
     //切入點表達式: execution(權限修飾 返回類型  增強方法所在類別的路徑 方法名稱(方法參數))
     //通知類型:
     // 前置 @Before(value = "切入點表達式 來配置 切入點")
-//    @Before(value = "execution(* com.spring.spring6.aop.annoAop.impl.*.*(..))")
-    @Before(value = "execution(public int com.spring.spring6.aop.annoAop.impl.CalculatorImpl.*(..))")
+//    @Before(value = "execution(public int com.spring.spring6.aop.annoAop.impl.CalculatorImpl.*(..))")
+    @Before("pointCut()")
     public void beforeMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
@@ -26,14 +27,14 @@ public class LogAspect {
 
     }
     // 後置 @After()
-    @After("execution(* com..*(..))")
+    @After("pointCut())")
     public void afterMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
         System.out.println("Logger-->後置通知, 方法名: " + methodName + ", 參數: " + Arrays.toString(args));
     }
     // 返回 @AfterReturning
-    @AfterReturning(value = "execution(* com..*(..))", returning = "result")
+    @AfterReturning(value = "pointCut())", returning = "result")
     public void afterReturning(JoinPoint joinPoint, Object result){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
@@ -46,7 +47,7 @@ public class LogAspect {
     }
     // 異常 @AfterThrowing 獲取目標方法異常訊息
     //目標方法出現異常, 這個通知執行
-    @AfterThrowing(value = "execution(* com..*(..))", throwing = "ex")
+    @AfterThrowing(value = "pointCut()", throwing = "ex")
     public void afterThrowing(JoinPoint joinPoint, Throwable ex){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
@@ -54,7 +55,7 @@ public class LogAspect {
         + " 異常訊息 = " + ex);
     }
     // 環繞 @Around()
-    @Around("execution(* com..*(..))")
+    @Around("pointCut()")
     public Object aroundMethod(ProceedingJoinPoint proceedingJoinPoint){
         String methodName = proceedingJoinPoint.getSignature().getName();
         Object[] args = proceedingJoinPoint .getArgs();
@@ -73,4 +74,8 @@ public class LogAspect {
         }
         return result;
     }
+    
+    //重複使用切入點表達式
+    @Pointcut("execution(* com.spring..*(..))")
+    private void pointCut(){}
 }
